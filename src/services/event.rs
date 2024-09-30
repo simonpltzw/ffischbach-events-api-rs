1ce2aceadbcadc7a05e77e5071a6fecc5ae87cc5
simonpltzw <github.karma467@passmail.net>
2024-09-30 23:06:48 +0200

    Created first custom params

--- src/services/event.rs
+++ src/services/event.rs
@@ -1,16 +1,32 @@
-use ntex::web;
+use ntex::web::{self};
+use serde::Deserialize;
+use utoipa::IntoParams;
+
+#[derive(Deserialize, IntoParams)]
+#[into_params(parameter_in = Query)]
+struct GetEventParams {
+  /// Limits the number of items returned from a collection. Defaults to 100.
+  top: Option<u32>,
+  /// Specifies the number of items to skip in a collection before returning the remaining items.
+  skip: Option<u32>
+}
 
 /// List all events
 #[utoipa::path(
   get,
   path = "/events",
+  params(GetEventParams),
   responses(
     (status = 200, description = "List of Events", body = [EventOutput]),
   ),
 )]
 #[web::get("/events")]
-pub async fn get_events() -> web::HttpResponse {
-  web::HttpResponse::Ok().finish()
+pub async fn get_events(query: web::types::Query<GetEventParams>) -> web::HttpResponse {
+  let top = query.top.unwrap_or(100);
+  let skip = query.skip.unwrap_or_default();
+
+
+  web::HttpResponse::Ok().content_type("application/json").body(format!("top: {}, skip: {}", top, skip))
 }
 
 /// Get an event by id
